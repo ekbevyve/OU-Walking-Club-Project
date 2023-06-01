1ecd59d1867c4c906368abdf85347155e033d275
Emily V <52414072+ekbevyve@users.noreply.github.com>
2023-06-01 01:08:57 +0100

    added in javascript code that does not work

--- add news.js
+++ add news.js
@@ -2,6 +2,10 @@ function NewsCount() {
     return Object.keys(localStorage).length;
 }
 
+function NewsClear() {
+    localStorage.clear();
+}
+
 function NewsSave() {
     // Grabbing all values from the news input form
     var title = document.getElementById('title').value;
@@ -15,22 +19,33 @@ function NewsSave() {
 
 function loadNewsEntries() {
     const container = document.getElementById('allnews');
-
+    container.innerHTML = "";
     for (var i = 0; i < localStorage.length; i++) {
         var key = localStorage.key(i);
         var newsItem = JSON.parse(localStorage.getItem(key));
 
         const section = document.createElement('div');
         section.setAttribute('class', 'newsitem');
-        section.innerHTML = `<h3>${newsItem.title}</h3><br><p>${newsItem.author}<br>${newsItem.body}</p>`;
+        section.innerHTML = `<h3>${newsItem.title}</h3><br><p>${newsItem.author}<br>${newsItem.body}</p> <button class="delete" id="${key}">delete</button>`;
         container.appendChild(section);
     }
+}
+
+function newsDelete(event) {
+    var newsId = event.target.id;
+    localStorage.removeItem(newsId);
+    event.target.parentNode.remove();
 }
 
 function initialize() {
     var saveButton = document.querySelector("#savecreate");
     saveButton.addEventListener("click", NewsSave);
     loadNewsEntries();
-}
-
+    var logOut = document.querySelector("#logout");
+    logOut.addEventListener("click", NewsClear);
+    var deleteItems = document.getElementsByClassName("delete");
+    for (var i = 0; i < deleteItems.length; i++) {
+      deleteItems[i].addEventListener("click", newsDelete);
+    }
+  }  
 window.addEventListener("load", initialize);
